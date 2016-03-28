0a2e4977a2791881026f4204deeb06eabbc40397
Douglas Cabral <douglas.cabral@fiap.com.br>
2016-03-28 16:16:26 -0300

    Finalização do cap. 9 do livro

--- src/CDC/Loja/FluxoDeCaixa/ProcessadorDeBoletos.php
+++ src/CDC/Loja/FluxoDeCaixa/ProcessadorDeBoletos.php
@@ -10,22 +10,10 @@ class ProcessadorDeBoletos
 {
     public function processa(ArrayObject $boletos, Fatura $fatura)
     {
-        $valorTotal = 0;
-        
-        $pagamentosFatura = $fatura->getPagamentos();
-        
         foreach ( $boletos as $boleto )
         {
             $pagamento = new Pagamento($boleto->getValor(), MeioPagamento::BOLETO);
-            $pagamentosFatura->append($pagamento);
-            
-            $valorTotal += $boleto->getValor();
+            $fatura->adicionaPagamento($pagamento);
         }
-        
-        if ( $valorTotal >= $fatura->getValor() )
-        {
-            $fatura->setPago(true);
-        }
-
     } 
 }
