76fcee2ac19ab2abbfb97fab5e2ea3b125cc1ca1
Douglas Cabral <douglas.cabral@fiap.com.br>
2016-03-28 16:10:40 -0300

    Testes apresentados no cap 9 do livro

---
+++ src/CDC/Loja/FluxoDeCaixa/ProcessadorDeBoletos.php
@@ -0,0 +1,31 @@
+<?php
+namespace CDC\Loja\FluxoDeCaixa;
+
+use CDC\Loja\FluxoDeCaixa\Fatura;
+use CDC\Loja\FluxoDeCaixa\Pagamento;
+use CDC\Loja\FluxoDeCaixa\MeioPagamento;
+use ArrayObject;
+
+class ProcessadorDeBoletos
+{
+    public function processa(ArrayObject $boletos, Fatura $fatura)
+    {
+        $valorTotal = 0;
+        
+        $pagamentosFatura = $fatura->getPagamentos();
+        
+        foreach ( $boletos as $boleto )
+        {
+            $pagamento = new Pagamento($boleto->getValor(), MeioPagamento::BOLETO);
+            $pagamentosFatura->append($pagamento);
+            
+            $valorTotal += $boleto->getValor();
+        }
+        
+        if ( $valorTotal >= $fatura->getValor() )
+        {
+            $fatura->setPago(true);
+        }
+
+    } 
+}
